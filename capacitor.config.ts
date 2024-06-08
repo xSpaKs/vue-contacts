066a6759aade258e7baf7c2337aa095bfe5931a9
Aran Hiblot <a.hiblot@student.isartdigital.com>
2024-06-08 14:01:59 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue3-contacts-ts',
+  webDir: 'dist'
+};
+
+export default config;
